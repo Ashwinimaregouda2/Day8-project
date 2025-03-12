0e7b7ac741d7b26e41f69d68cc5028fd3779b3a7
ashwini <ashwini.maregouda@gmail.com>
2025-03-12 09:36:53 +0000

    new

--- main.tf
+++ main.tf
@@ -7,26 +7,61 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
-# Subnets
-resource "aws_subnet" "public" {
+# Public Subnets
+resource "aws_subnet" "public1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
+  availability_zone       = "ap-southeast-2a"
 }
 
+resource "aws_subnet" "public2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "ap-southeast-2b"
+}
+
+# Private Subnet
 resource "aws_subnet" "private" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.2.0/24"
 }
 
-# Security Group
+# Security Group for ALB
 resource "aws_security_group" "alb_sg" {
   vpc_id = aws_vpc.main.id
-  
+
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for Instances
+resource "aws_security_group" "instance_sg" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.alb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
@@ -37,17 +72,10 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  
-  subnets = [
-    aws_subnet.public1.id,
-    aws_subnet.public2.id
-  ]
+  subnets           = [aws_subnet.public1.id, aws_subnet.public2.id]
 
   enable_http2 = true
 }
-
-
-
 
 # Target Group
 resource "aws_lb_target_group" "app_tg" {
@@ -57,32 +85,55 @@ resource "aws_lb_target_group" "app_tg" {
   vpc_id   = aws_vpc.main.id
 }
 
-# Auto Scaling Group
+# Listener for ALB
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app_lb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.app_tg.arn
+  }
+}
+
+# Launch Template
 resource "aws_launch_template" "app_lt" {
   name_prefix   = "app-lt"
   image_id      = "ami-09e143e99e8fa74f9"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+
+  user_data = base64encode(<<EOF
+#!/bin/bash
+sudo apt update -y
+sudo apt install -y apache2
+sudo systemctl start apache2
+sudo systemctl enable apache2
+echo "Hello from Terraform" > /var/www/html/index.html
+EOF
+  )
 }
 
+# Auto Scaling Group
 resource "aws_autoscaling_group" "app_asg" {
   desired_capacity     = 2
   max_size            = 3
   min_size            = 1
-  vpc_zone_identifier = [aws_subnet.public.id]
+  vpc_zone_identifier = [aws_subnet.public1.id, aws_subnet.public2.id]
+
   launch_template {
     id      = aws_launch_template.app_lt.id
-    version = "$Latest"
+    version = "$${aws_launch_template.app_lt.latest_version}"
   }
+
+  target_group_arns = [aws_lb_target_group.app_tg.arn]
 }
 
 # S3 Bucket
 resource "aws_s3_bucket" "static_content" {
-
   bucket = "my-static-content-bucket1235"
-
 }
-
-
 
 # CloudWatch Logs
 resource "aws_cloudwatch_log_group" "app_logs" {
